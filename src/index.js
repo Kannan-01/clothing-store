c9e4e8766440781fb02a2f548542b943dd9d333c
Code-Geeek <dreampersonalc@gmail.com>
2023-12-02 09:43:53 +0530

    'completed'

--- src/index.js
+++ src/index.js
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import './bootstrap.min.css';
-import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './Pages/redux/store';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}><BrowserRouter> <App /> </BrowserRouter></Provider>
+    <Provider store={store}><BrowserRouter><App /></BrowserRouter></Provider>
+    
   </React.StrictMode>
 );
 
